2db46e050ed99f260db4803cb3561e5c5cf6d001
jysh1214 <jyxemperor@gmail.com>
2021-01-17 01:21:47 +0800

    region point

--- region_point.py
+++ region_point.py
@@ -1,14 +1,61 @@
 import numpy as np
 
-"""
-@image: rawdata of image (numpy array)
-@size: width, height of image
-"""
 def region_point(image, size):
-    print(image.size)
-    print(size[0])
-    print(size[1])
+    # print(image.size)
+    # print(size[0])
+    # print(size[1])
     data = np.array(image)
-    # for i in range(10):
-    #     for j in range(10):
-    #         print(data[i, j])
+
+    startp = (0, 0)
+    # find first point
+    done = False
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if (data[i, j] == True):
+                startp = (j, i)
+                done = True
+                break
+        if done:
+            break
+
+    all_points = []
+    j = startp[0]
+    i = startp[1]
+
+    print(startp)
+
+    # 右
+    down = False
+    while (True):
+        if (data[i, j-1] == True and not(down)):
+            all_points.append((j, i))
+            j = j - 1
+            print("Current Point: ", j, i)
+            continue
+        elif (data[i+1, j-1] == True):
+            all_points.append((j, i))
+            j = j - 1
+            i = i + 1
+            print("Current Point: ", j, i)
+            continue
+        elif (data[i+1, j] == True):
+            all_points.append((j, i))
+            i = i + 1
+            print("Current Point: ", j, i)
+            continue
+        elif (data[i+1, j+1] == True):
+            all_points.append((j, i))
+            j = j + 1
+            i = i + 1
+            print("Current Point: ", j, i)
+            continue
+        elif (data[i, j+1] == True):
+            all_points.append((j, i))
+            j = j + 1
+            print("Current Point: ", j, i)
+            down = True
+            continue
+        elif ((j, i) in all_points):
+            break
+        else:
+            break
